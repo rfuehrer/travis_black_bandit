eaa5d16d9d54a70dc7b56147352b30c93697f6f5
Rene Fuehrer <rene.fuehrer@generali.com>
2020-08-07 06:06:21 +0200

    *

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hello
+![Travis CI](https://travis-ci.com/ssaru/travis-ci-with-flake8.svg?branch=master)
---
+++ main.py
@@ -0,0 +1,15 @@
+
+def something(i):
+
+    return i
+
+if __name__ == "__main__":
+    print("Hello World")
+
+    for i in range(100):
+
+
+
+        print(something(i))
+        
+
